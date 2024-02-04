5c646cc65161deace842bb27dff07f3479c2e8ba
Thomas Hayter <tomhayter24@gmail.com>
2024-02-04 16:50:39 +0000

    Add static root

--- bookclub/settings.py
+++ bookclub/settings.py
@@ -118,6 +118,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = []
+
+# Following settings only make sense on production and may break development environments.
+if not DEBUG:    
+    # Tell Django to copy statics to the `staticfiles` directory
+    # in your application directory on Render.
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Turn on WhiteNoise storage backend that takes care of compressing static files
+    # and creating unique names for each version so they can safely be cached forever.
+    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
